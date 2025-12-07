9a04052227873910268aa15aa6d7a9bd8fb5cdac
Nitish <Nitish>
2025-12-07 15:44:34 +0530

    added React packages and removed CDN support

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-const parent = React.createElement("div", {id: "parent"},
-    React.createElement("div", {id: "child"},
-        [React.createElement("h1", {}, "I am h1 tag"),
-        React.createElement("h2", {}, "I am h2 tag"),
-        React.createElement("h3", {}, "I am h3 tag")]
-    )
-)
-console.log(parent);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.render(parent, document.getElementById("root"));
-// Document.getElementById("root").appendChild(parent);
+console.log("React verstion is ", React.version);
+// React Element - Object representation of UI
+const heading  = React.createElement("h1",{},"Hello World from React");
+
+const paragraph = React.createElement("div",{id:"parent"},
+    React.createElement("div", {id:"child"},[
+        React.createElement("h1",{key:"h1"},"I am a h1 tag inside div"),
+        React.createElement("h2",{key:"h2"},"I am a h2 tag inside div")
+    ])
+)
+
+// Render Method
+const headingRoot = ReactDOM.createRoot(document.getElementById("heading"));
+const paraRoot = ReactDOM.createRoot(document.getElementById("root"));
+
+headingRoot.render(heading);
+paraRoot.render(paragraph);
