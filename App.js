b7dedd07101869795db2cd9dc5d0e3b87dfb6e89
Nitish <Nitish>
2025-12-07 20:42:34 +0530

    feat: add Javascript code in JSX
    In this commit we will understand how to add Javascript code inside the JSX component

--- App.js
+++ App.js
@@ -1,6 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const products = [
+    {
+        name: "Product 1",
+        price: 1000,
+        description: "This is product 1",
+        inStock : true
+
+    },
+    {
+        name: "Product 2",
+        price: 2000,
+        description: "This is product 2",
+        inStock : false
+    },
+    {
+        name: "Product 3",
+        price: 3000,
+        description: "This is product 3",
+        inStock : true
+    }
+];
+
 const TitleComponent = () => {
     return (
         <div>
@@ -8,15 +30,34 @@ const TitleComponent = () => {
         </div>
     );
 }
-
+const heading = <h1>Welcome to Namaste React</h1>;
+// Component Composition
 const CardComponent = () => {
     return (
         <div className="card">
             <TitleComponent />
             <p>This is a description of the card.</p>
+            <h3>Price: {number} INR</h3>
+            
+            
         </div>
+    );
+}
+const DisplayProducts = () => {
+    return (
+        <div className="product-container">
+            {heading}
+           <div className="product-card">
+             {products.map((product,i)=> {
+                return (
+                    <h3 key={`p-${i}`}>{product.name} - {product.price} INR - {product.inStock ? "In Stock" : "Out of Stock"}</h3>
+                );
+             })}
+           </div>
+        </div>
+            
     );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<CardComponent />);
+root.render(<DisplayProducts />);
