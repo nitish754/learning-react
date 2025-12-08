0670eb0fe2fe364a6caa53e2eae5753812b281fe
Nitish <Nitish>
2025-12-08 19:33:03 +0530

    Passing Props to the component

--- Components/CircleImage.jsx
+++ Components/CircleImage.jsx
@@ -1,28 +1,13 @@
-const CircleImage = () => {
+const CircleImage = (props) => {
+    const img = props.image || "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/8f508de7-e0ac-4ba8-b54d-def9db98959e_burger.png"
     return (
-        <div className="circle-image">
+        <div >
             <img 
-                src="https://images.rawpixel.com/image_png_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbGlmZW9mcGl4MDAwMDEtaW1hZ2VfMS1renhsdXd3ci5wbmc.png" 
+                src={img} 
                 alt="Circle" 
                 className="circle-img"
             />
-            <img 
-                src="https://images.rawpixel.com/image_png_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbGlmZW9mcGl4MDAwMDEtaW1hZ2VfMS1renhsdXd3ci5wbmc.png" 
-                alt="Circle" 
-                className="circle-img"
-            />
-            <img 
-                src="https://images.rawpixel.com/image_png_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbGlmZW9mcGl4MDAwMDEtaW1hZ2VfMS1renhsdXd3ci5wbmc.png" 
-                alt="Circle" 
-                className="circle-img"
-            />
-            <img 
-                src="https://images.rawpixel.com/image_png_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbGlmZW9mcGl4MDAwMDEtaW1hZ2VfMS1renhsdXd3ci5wbmc.png" 
-                alt="Circle" 
-                className="circle-img"
-            />
-          
-           
+         
         </div>
     );
 }
--- Components/TopRestaurant.jsx
+++ Components/TopRestaurant.jsx
@@ -5,9 +5,9 @@ const TopRestaurant = () => {
         <div className="top-restaurant-conatiner">
             <h2>Top Restaurants For You 🥯</h2>
             <div className="top-restaurant-list">
-                <RestaurantCard name="The Gourmet Kitchen" cuisine="Italian" rating="4.8" />
-                <RestaurantCard name="Sushi World" cuisine="Japanese" rating="4.7" />
-                <RestaurantCard name="Curry Palace" cuisine="Indian" rating="4.6" />
+                <RestaurantCard name="The Gourmet Kitchen" cuisine="Italian" rating="4.8" image="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/9/1/b314734e-e994-4ffb-a2f9-e2c9627cbfaa_377797.JPG" />
+                <RestaurantCard name="Sushi World" cuisine="Japanese" rating="4.7" image="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/11/11/c7bfd952-2ec9-4ef2-9a9a-68d2f0361410_747134.JPG" />
+                <RestaurantCard name="Curry Palace" cuisine="Indian" rating="4.6" image="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/6/18/2889c7c2-de06-4e50-9808-7e1075b6a5d9_34932.jpg" />
                 <RestaurantCard name="Burger Hub" cuisine="American" rating="4.5" />
        
 
