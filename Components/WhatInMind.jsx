09290f2f8d9e7e9c44696bca7cdbc6df0c3c075c
Nitish <Nitish>
2025-12-09 17:22:54 +0530

    Render the data dynamically via props from dummy JSON file

--- Components/WhatInMind.jsx
+++ Components/WhatInMind.jsx
@@ -1,15 +1,17 @@
 import CircleImage from "./CircleImage";
 
-const WhatsInMind = () => {
+const WhatsInMind = ({data}) => {
     return (
         <section className="what-in-mind">
             <h2>What's on Your Mind?</h2>
             <p>Pick up the foods you might like!</p>
             <div className="circle-image">
-                <CircleImage name="Burgers" image="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/8f508de7-e0ac-4ba8-b54d-def9db98959e_burger.png"/>
-            <CircleImage name="Pizzas" image="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/6ef07bda-b707-48ea-9b14-2594071593d1_Pizzas.png"/>
-            <CircleImage name="Sushi" image="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/8f508de7-e0ac-4ba8-b54d-def9db98959e_cake.png"/>
-            <CircleImage name="Desserts" image="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/6ef07bda-b707-48ea-9b14-2594071593d1_Biryani.png"/>
+                {
+                    data.map((item) => {
+                        return <CircleImage key={item.id} name={item.name} image={item.image} />
+                    })
+                }
+               
             </div>
         </section>
     );
--- Pages/Home.jsx
+++ Pages/Home.jsx
@@ -2,13 +2,16 @@ import Header from "../Components/Header";
 import WhatsInMind from "../Components/WhatInMind";
 import TopRestaurant from "../Components/TopRestaurant";
 import Footer from "../Components/Footer";
+import WhatsInMindData from "../utils/whatsInMind.json"
+import restaurants from "../utils/restaurant.json"
+
 const Home = () => {
     return (
         <div className="home-container">
             <Header />
             <div className="home-content">
-                <WhatsInMind />
-                <TopRestaurant />
+                <WhatsInMind data={WhatsInMindData}/>
+                <TopRestaurant restro={restaurants}/>
 
                 <Footer />
             </div>
